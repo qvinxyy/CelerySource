4d45346bf59627777e5317b760a0b9541bf40934
qvinxyy <91805571+qvinxyy@users.noreply.github.com>
2023-05-18 07:55:21 -0500

    Add files via upload

---
+++ Misc/Miscellaneous.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CeleryApp.Misc
+{
+	public static class Miscellaneous
+	{
+		public static T GetTemplateChild<T>(this Control e, string name) where T : FrameworkElement
+		{
+			return e.Template.FindName(name, e) as T;
+		}
+	}
+}
